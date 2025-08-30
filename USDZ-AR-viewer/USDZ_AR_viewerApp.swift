33ca205dbb33c74e806db638e65e742e1bf9ab6f
Remo Prozzillo <55060292+RemoPro@users.noreply.github.com>
2025-08-30 22:13:03 +0200

    Initial Commit

---
+++ USDZ-AR-viewer/USDZ_AR_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  USDZ_AR_viewerApp.swift
+//  USDZ-AR-viewer
+//
+//  Created by Remo on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct USDZ_AR_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
